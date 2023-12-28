9f62e8e156ce7a43f77ed8c00c8d0bc62fdf214f
K-KY <66647057+K-KY@users.noreply.github.com>
2023-12-28 20:00:36 +0900

    Feat : SpringInitialCommit

---
+++ creavispace/src/main/java/creavispace/project/CreavispaceApplication.java
@@ -0,0 +1,13 @@
+package creavispace.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreavispaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreavispaceApplication.class, args);
+	}
+
+}
---
+++ creavispace/src/test/java/creavispace/project/CreavispaceApplicationTests.java
@@ -0,0 +1,13 @@
+package creavispace.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreavispaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
